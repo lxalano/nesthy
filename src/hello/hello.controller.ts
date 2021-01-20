99ed48512c0180bfeaf1e0b0d7ba734264ace074
dafei <df@example.com>
2021-01-20 18:41:24 +0800

    提交

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Query, Headers } from '@nestjs/common';
-import { get } from 'http';
+import { Controller, Get, Query, Headers, Post, HttpCode, Body, Patch } from '@nestjs/common';
+import { MESSAGES } from '@nestjs/core/constants';
 import { HelloService } from './hello.service';
 
 @Controller('hello')
 export class HelloController {
     constructor(private readonly helloService: HelloService) { }
-
+    //查询
     @Get()
     fetch(@Query() { id }, @Headers('token') token): string {
         console.log(token);
         return this.helloService.fatch(id);
     }
+    //创建
+    @Post()
+    @HttpCode(201)
+    create(@Query() { id }, @Body() { message }): string {
+        console.log(id)
+        return this.helloService.create(message);
+    }
+    //更新
+    @Patch(': id')
+    update(@Query() { id }, @Body() { message }): string {
+        return this.helloService.update(id, message);
+    }
+
+
 }
--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -5,6 +5,12 @@ export class HelloService {
     fatch(id: any): string {
         return `Hello int(${id})`;
     }
+    create(message): string {
+        return `create Hello Done.(${message})`;
+    }
+    update(id: string, message: string): string {
+        return `Set Hello Done.(${id}:${message})`;
+    }
 
 
 }
